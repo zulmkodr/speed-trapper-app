ca4f8947539281ae645467724513986d12c36a6f
kaushik19 <kaushikpathak19@gmail.com>
2021-06-03 10:45:11 +0530

    solution of the speed trap problem

---
+++ car-app/CarApp/CarApp/Filters/InitializeSimpleMembershipAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Threading;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+
+namespace CarApp.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public sealed class InitializeSimpleMembershipAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // Ensure ASP.NET Simple Membership is initialized only once per app start
+            //LazyInitializer.EnsureInitialized(ref _initializer, ref _isInitialized, ref _initializerLock);
+        }
+    }
+}
---
+++ car-app/CarApp/CarApp/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarApp.Startup))]
+namespace CarApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HubConfiguration();
+            config.EnableJSONP = true;
+            config.EnableDetailedErrors = true;
+            app.MapSignalR(config);
+        }
+    }
+}
